3d922e4037fd000497cb12bfb09a77579ef817f3
Matthew Foyle <matt@moltin.com>
2017-08-20 09:47:54 +0100

    Abstract the fetching of moltin products into separate action creator

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,7 @@
-export const Update_Quantity = 'Update_Quantity'
+export const Update_Quantity = 'Update_Quantity';
+export const Fetch_Products_Start = 'Fetch_Products_Start';
+export const Fetch_Products_End = 'Fetch_Products_End';
+var api = require('../utils/moltin.js');
 
 export function updateQuantity(quantity) {
   return {
@@ -7,3 +10,28 @@ export function updateQuantity(quantity) {
   }
 };
 
+export function FetchProductsStart() {
+  return {
+    type: Fetch_Products_Start
+  }
+};
+
+export function FetchProductsEnd(data) {
+  return {
+    type: Fetch_Products_End,
+    payload: data
+  }
+};
+
+export function GetProducts() {
+  return function (dispatch) {
+      
+      dispatch(FetchProductsStart())
+
+      return api.GetProducts()
+      
+        .then((products) => {
+          dispatch(FetchProductsEnd(products))
+        }) 
+    }
+};
