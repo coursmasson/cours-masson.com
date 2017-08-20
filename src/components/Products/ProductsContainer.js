33002ebb39ffe67cdedc630a0a08a86d0a2869d5
Matthew Foyle <matt@moltin.com>
2017-08-20 09:50:30 +0100

    apply get products action creator to the products listing page

--- src/components/Products/ProductsContainer.js
+++ src/components/Products/ProductsContainer.js
@@ -5,7 +5,7 @@ import ProductsHeader from './ProductsHeader';
 import MobileNav from '../global/Mobile/MobileNav';
 import { connect } from 'react-redux';
 import Loading from '../global/Loading';
-var api = require('../../utils/moltin.js');
+import { GetProducts } from '../../actions/actions.js';
 
 const mapStateToProps = state => {
   return {
@@ -28,24 +28,16 @@ class ProductsContainer extends Component {
   // a react lifecycle event, read more at http://busypeoples.github.io/post/react-component-lifecycle/
   componentDidMount() {
 
+    const boundGetProducts = () => {
+      this.props.dispatch((dispatch) => {
+          dispatch(GetProducts())
+      })
+    }
+    
+    
     // check if we already have a moltin products in the store
     if(this.props.products.fetched === false) {
-
-      // dispatch an action to our redux reducers
-      this.props.dispatch((dispatch) => {
-
-          // this action will set a fetching field to true
-          dispatch({type: "Fetch_Products_Start"})
-
-          // get the moltin products from the API
-          api.GetProducts()
-
-          .then((products) => {
-            /* now that we have the products, this action will set fetching to false and fetched to true,
-            as well as add the moltin products to the store */
-            dispatch({type: "Fetch_Products_End", payload: products})
-          })
-      })
+      boundGetProducts()
     }
   }
 
