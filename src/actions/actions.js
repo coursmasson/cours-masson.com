6a4469f856163bf9e374651c8c5ddac202e9fb05
Matthew Foyle <matt@moltin.com>
2017-08-20 10:24:07 +0100

    New abstracted action creators for getting categories and initialising a style (category)

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,10 @@
 export const Update_Quantity = 'Update_Quantity';
 export const Fetch_Products_Start = 'Fetch_Products_Start';
 export const Fetch_Products_End = 'Fetch_Products_End';
+export const Fetch_Categories_Start = 'Fetch_Categories_Start';
+export const Fetch_Categories_End = 'Fetch_Categories_End';
+export const Initial_Style = 'Initial_Style';
+
 var api = require('../utils/moltin.js');
 
 export function updateQuantity(quantity) {
@@ -35,3 +39,37 @@ export function GetProducts() {
         }) 
     }
 };
+
+export function FetchCategoriesStart() {
+  return {
+    type: Fetch_Categories_Start
+  }
+};
+
+export function FetchCategoriesEnd(data) {
+  return {
+    type: Fetch_Categories_End,
+    payload: data
+  }
+};
+
+export function GetCategories() {
+  return function (dispatch) {
+      
+      dispatch(FetchCategoriesStart())
+
+      return api.GetCategories()
+      
+        .then((categories) => {
+          dispatch(FetchCategoriesEnd(categories))
+        }) 
+    }
+};
+
+export function InitialStyle(style) {
+  return {
+    type: Initial_Style,
+    style: style,
+    header: style
+  }
+}
--- src/components/Styles/StylesContainer.js
+++ src/components/Styles/StylesContainer.js
@@ -8,8 +8,9 @@ import StyleProducts from './StyleProducts';
 import Loading from '../global/Loading';
 import { connect } from 'react-redux';
 import MobileNav from '../global/Mobile/MobileNav';
-
-var api = require('../../utils/moltin.js');
+import { GetProducts } from '../../actions/actions.js';
+import { GetCategories } from '../../actions/actions.js';
+import { InitialStyle } from '../../actions/actions.js';
 
 function mapStateToProps(state) {
     return(state)
@@ -30,40 +31,44 @@ class StylesContainer extends Component {
   // a react lifecycle event, read more at http://busypeoples.github.io/post/react-component-lifecycle/
   componentDidMount() {
 
-    // check if we already have a moltin products in the store
-    if(this.props.products.fetched === false) {
+    const boundInitialStyle = () => {
       this.props.dispatch((dispatch) => {
+          dispatch(InitialStyle(this.props.categories.categories.data[0].name))
+    })
+  }
 
-          dispatch({type: "Fetch_Products_Start"})
-
-          api.GetProducts()
-
-          .then((products) => {
-            dispatch({type: "Fetch_Products_End", payload: products})
+    
+    const boundGetCategoriesAndInitialStyle = () => {
+      this.props.dispatch((dispatch) => {
+          dispatch(GetCategories())
+          .then(() => {
+            boundInitialStyle()
           })
       })
     }
 
-    // now we do the same thing for categories
-    if(this.props.categories.fetched === false) {
+
+    const boundGetProducts = () => {
       this.props.dispatch((dispatch) => {
-        dispatch({type: "Fetch_Categories_Start"})
-
-        api.GetCategories()
-
-        .then((categories) => {
-          dispatch({type: "Fetch_Categories_End", payload: categories})
-          if(categories.data.length > 0) {
-              dispatch({type: "Initial_Style", style: categories.data[0].name, header: categories.data[0].name})
-          }
-        
-        })
+          dispatch(GetProducts())
       })
+    }
+    
+    // check if we already have a moltin products in the store
+    if(this.props.products.fetched === false) {
+      boundGetProducts()
+    }
+    
+    // check if we already have a moltin products in the store
+    if(this.props.categories.fetched === false) {
+      boundGetCategoriesAndInitialStyle();  
+  
     }
 
   }
 
   render() {
+
     if(this.props.categories.categories && this.props.products.products) {
       
         if(this.props.categories.categories.data.length > 0) {
